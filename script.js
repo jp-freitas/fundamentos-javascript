464966448707d02944af2e48846e2b9e18d501e6
João Pedro de Deus Freitas <tec.joao.freitas@gmail.com>
2026-05-21 18:39:10 +0000

    Inicios de fundamentos de Javascript

---
+++ script.js
@@ -0,0 +1,25 @@
+// Isso aqui é um comentário
+
+/**
+ * Este é um
+ * comentário de
+ * múltiplas linhas
+ */
+
+var nome;
+let data_nascimento = "1994-08-27";
+// const CPF = 00000000000;
+
+let num1 = 2;
+let num2 = 5;
+
+let soma = num1 + num2;
+
+console.log(soma);
+
+// Conectivo lógico? E OU Nao
+// AND OR NOT - && || !
+
+if (!nome) {
+    console.log("Não está preenchido")
+}
